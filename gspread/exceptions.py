ed190d318b84148c384c173339dce1371fab2b88
Lavigne958 <lavigne958@gmail.com>
2020-04-06 19:54:18 +0200

    Improve APIError messages
    - Extract return code
    - Extract message
    - Build quick and simple message combining both
    
    The caller can compare the return code if necessary it is cast to
    an integer, the message is extracted as well for the caller
    to be able to display/compare them.
    
    The original Exception args contain a string ready to be displayed.
    
    Signed-off-by: Lavigne958 <lavigne958@gmail.com>

--- gspread/exceptions.py
+++ gspread/exceptions.py
@@ -37,14 +37,20 @@ class APIError(GSpreadException):
     def __init__(self, response):
 
         super(APIError, self).__init__(self._extract_text(response))
-        self.response = response
 
     def _extract_text(self, response):
         return self._text_from_detail(response) or response.text
 
     def _text_from_detail(self, response):
         try:
-            errors = response.json()
-            return errors['error']
+            errors = response.json()["error"]
+            self.return_code = int(errors["code"])
+            self.message = errors["message"]
+
+            # The detailed section "errors" is only here on errors from google drive API,
+            # not in errors from google spreadsheet API
+            self.details = errors.get("errors") or ""
+
+            return "({}) - {}".format(self.return_code, self.message)
         except (AttributeError, KeyError, ValueError):
             return None
